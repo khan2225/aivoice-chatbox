d6da50a4f1415b4069fdae832f3d4387c73d8c3a
khan2225 <142520580+khan2225@users.noreply.github.com>
2025-03-31 17:09:47 -0400

    Update index.js

--- aivoice-chatbox-main/index.js
+++ aivoice-chatbox-main/index.js
@@ -67,7 +67,7 @@ fastify.all('/incoming-call', async (request, reply) => {
 
     const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
                           <Response>
-                              <Say>Hi, you have called Bart's Automative Centre. How can we help?</Say>
+                              <Say>Hi, thanks for reaching out. I'm very interested in what you have to offer.</Say>
                               <Connect>
                                   <Stream url="wss://${request.headers.host}/media-stream" />
                               </Connect>
