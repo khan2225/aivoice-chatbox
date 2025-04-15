10236e01e8494357ee88b1f5db125e85c89002b3
saniyah khan <s_khan11@uncg.edu>
2025-04-14 21:42:46 -0400

    port issues fixed.

--- routes/incoming-call.js
+++ routes/incoming-call.js
@@ -11,7 +11,7 @@ export function handleIncomingCall(fastify){
         const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
                               <Response>
                                   <Connect>
-                                      <Stream url="wss://${request.headers.host}/media-stream?persona=${persona}" />
+                                      <Stream url="wss://${host}/media-stream?persona=${persona}" />
                                   </Connect>
                               </Response>`;
     
