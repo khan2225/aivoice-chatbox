a71c134ac9800155b78dbac3fa374e4ca7f374c7
saniyah khan <s_khan11@uncg.edu>
2025-04-15 23:41:55 -0400

    personas added.

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ fastify.all("/incoming-call", async (request, reply) => {
     const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
                           <Response>
                               <Connect>
-                                  <Stream url="wss://${request.headers.host}/media-stream?persona=${personaKey}" />
+                                  <Stream url="wss://${request.headers.host}/media-stream%3Fpersona%3D${personaKey}" />
                               </Connect>
                           </Response>`;
 
